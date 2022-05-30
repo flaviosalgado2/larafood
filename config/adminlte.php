5a3cd5ec05ed34f0925e461cc396f084508cfe3f
flaviosalgado <flavio@canaltelecom.com.br>
2022-05-29 23:17:36 -0300

    parte inicial de perfis

--- config/adminlte.php
+++ config/adminlte.php
@@ -258,6 +258,11 @@ return [
             'url'  => 'admin/plans',
             'icon' => 'fas fa-list-alt'
         ],
+        [
+            'text' => 'Perfis',
+            'url'  => 'admin/profiles',
+            'icon' => 'fas fa-user'
+        ]
     ],
 
     /*
---
+++ resources/views/admin/pages/profiles/index.blade.php
@@ -0,0 +1,58 @@
+@extends('adminlte::page')
+
+@section('title', 'Perfis')
+
+@section('content_header')
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
+        <li class="breadcrumb-item"><a href="{{ route('profiles.index') }}" class="active">Perfis</a></li>
+    </ol>
+
+    <h1>Perfis <a href="{{ route('profiles.create') }}" class="btn btn-dark"><i class="fas fa-plus-square"></i> ADD</a></h1>
+@stop
+
+@section('content')
+    <div class="card">
+
+        <div class="card-header">
+            <form action="{{ route('plans.search') }}" method="POST" class="form form-inline">
+                @csrf
+                <input type="text" placeholder="Nome" name="filter" class="form-control" value="{{ $filters['filter'] ?? '' }}">
+                <button type="submit" class="btn btn-dark">Filtrar</button>
+            </form>
+        </div>
+
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+
+                        <th width="150">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($profiles as $profile)
+                        <tr>
+                            <td>
+                                {{ $profile->name }}
+                            </td>
+
+                            <td style="width: 10px;">
+                                <a href="{{ route('profiles.edit', $profile->id) }}" class="btn btn-info">Editar</a>
+                                <a href="{{ route('profiles.show', $profile->id) }}" class="btn btn-warning">Ver</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <div class="card-footer">
+            @if (isset($filters))
+                {!! $profiles->appends($filters)->links() !!}
+            @else
+                {!! $profiles->links() !!}
+            @endif
+        </div>
+    </div>
+@stop
